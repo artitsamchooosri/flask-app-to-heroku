f3288a060b396ee3b999bccc4476a3bf6e89b10d
artitsamchooosri <33492904+artitsamchooosri@users.noreply.github.com>
2022-08-05 09:32:21 +0700

    firat

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, make_response
+from flask_bootstrap import Bootstrap
+import os
+import uuid
+import base64
+
 import json
 import numpy as np
 import pandas as pd
 import requests
+
+from PIL import Image
+import warnings
+warnings.simplefilter('error', Image.DecompressionBombWarning)
 
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage, FlexSendMessage
@@ -12,7 +21,8 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='imgs')
+bootstrap = Bootstrap(app)
 
 lineaccesstoken = 'gyBAJgxWYGkpKNPDnsuhmW4veeCqOO9kzPv5SGNkhaysTCWXEFwVCtYWGQFstVaaKMGoSoFU/T3PyHrejvHQUzjTQZ/kfKRh06zwfksY9wPlvKPznqom5N0exYWU/aMJU4c9OdMXgbKpczOg6TpTvgdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(lineaccesstoken)
@@ -21,7 +31,31 @@ line_bot_api = LineBotApi(lineaccesstoken)
 
 @app.route('/')
 def index():
-    return "Hello Worl"
+    return render_template('index.html')
+
+@app.route('/saveimage', methods=['POST'])
+def saveimage():
+  event = request.form.to_dict()
+
+  dir_name = 'imgs'
+  img_name = uuid.uuid4().hex
+
+  # Saving image in the 'imgs' folder temporarily. Should be deleted after a certain period of time
+  if not os.path.exists(dir_name):
+      os.makedirs(dir_name)
+  with open(os.path.join(dir_name, '{}.jpg'.format(img_name)), 'wb') as img:
+      img.write(base64.b64decode(event['image'].split(",")[1]))
+
+  original = Image.open(os.path.join(dir_name, '{}.jpg'.format(img_name)))
+  # Needs simple validation of format for security since Pillow supports various type of Images
+  if(original.format != 'JPEG'):
+      return make_response('Unsupported image type.', 400)
+
+  original.thumbnail((240, 240), Image.ANTIALIAS)
+  original.save(os.path.join(dir_name, '{}_240.jpg'.format(img_name)), 'JPEG')
+
+  return make_response(img_name, 200)
+
 
 @app.route('/testpush')
 def testpush():
